4a655ae6617c391996389ce44f5d4838c8e301fb
elyasaf waisman <elyasafw1997@gmail.com>
2026-09-17 12:22:19 +0300

    add routes to App file and create basic protected route component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <ProfilePage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>... דף לא קיים</h2>
+        </div>
+    );
+};
+
+export default NotFoundPage;
